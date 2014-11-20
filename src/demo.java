616219d72fa334acf41c1243a1fa276a32a9b839
Ashish <akhatkar64@gmail.com>
2014-11-21 01:20:38 +0530

    tree_created_using_beam_search

--- src/demo.java
+++ src/demo.java
@@ -5,7 +5,10 @@ public class demo
 {
 	public static void main(String[] args) 
 	{
-		Tree_Making tm=new Tree_Making();
+		Board b = new Board(4);
+		Beam_Search bs = new Beam_Search();
+		bs.beam_search(b);
+		/*Tree_Making tm=new Tree_Making();
 		tm.construct_tree();
 		System.out.println(tm.get_size());
 		/*Board b = new Board();
